15acec90b1b601889d58af9eb821e7dd3ca1991c
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2023-10-01 20:30:57 +0530

    Commited

--- src/com/company/ProductService.java
+++ src/com/company/ProductService.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductService {
+
+    // below here in angle brackets it is called the generic type
+    // generic types means the user-defined class or methods
+    // it means list of values are stored in the product named and the Product which is already pre-defined by the user is used
+    // as generic type so that we can further access the methods inside the Product class
+
+    // below in the for loop I have used the Product p so we have the access to the getName() method
     List<Product> product = new ArrayList<>();
     public void addProduct(Product p){
         product.add(p);
